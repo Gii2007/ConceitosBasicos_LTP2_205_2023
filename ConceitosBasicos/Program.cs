f627eddf340ee688fb6df7ecf74e0016341a8f34
Aula <Aula@CANP-LABTI-C02>
2023-03-30 15:41:22 -0300

    Adicionar arquivos de projeto.

---
+++ ConceitosBasicos/Program.cs
@@ -0,0 +1,23 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Hello, World!");
+string nome = "seu nome";
+Console.WriteLine($"seja bemVindo(a){nome}");
+string cidade = "Ipiabas";
+Console.WriteLine($"eu moro em {cidade}");
+
+int idade = 16;
+Console.WriteLine($"minha idade {idade}");
+byte valor = 255;
+valor += 1;// valor = valor +1;
+Console.WriteLine($"valor = {valor}");
+decimal salario = 100000.50m;
+float altura = 1.64f;
+float peso = 62.5f;
+double altura = altura * 0.0000000001;
+float altura = 1.76f;
+double alturaN = *0.000000001;
+Console.WriteLine($"sua altura é {altura}");
+Console.WriteLine($" se voce pudesse ser o homen formiga, teria a altura {alturaN}nm");
+Console.WriteLine($" o peso informado foi{peso}");
+
+
